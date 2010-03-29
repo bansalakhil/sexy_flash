276c3932bc0e869ef12ad09074633ce78d6d8fbf
Akhil Bansal <bansalakhil30.10@gmail.com>
2010-03-29 15:47:45 +0530

    Did changes to make it Rails3 compatible.

--- lib/sexy_flash_view_helper.rb
+++ lib/sexy_flash_view_helper.rb
@@ -42,7 +42,7 @@ module SexyFlashViewHelper
         end #if timeout > 0
       end #if flash.has_key?(key)
     end #each do |key|
-    return the_flash
+    return the_flash.try(:html_safe)
   end 
 
 end
